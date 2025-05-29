b39abc12d9eaf7cce5fbdca9ecb6cce152b8ef01
business221-Xo <auction221business@gmail.com>
2025-05-29 17:42:22 -0400

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Your Assistant-scroll
+          Your Assistant
         </a>
       </header>
 
